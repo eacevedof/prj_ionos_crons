762749b0868d8a0d97002a6953cd7537878ef5ee
ioedumac <eacevedof@yahoo.es>
2020-08-28 17:42:47 +0200

    mejoro ruta de cron

--- backend/src/Controllers/MainController.php
+++ backend/src/Controllers/MainController.php
@@ -3,9 +3,16 @@ namespace App\Controllers;
 
 class MainController
 {
+    private const FOLDER_CRONS = "";
+
+    private function _get_cronpath($filename){
+        return self::FOLDER_CRONS."/".$filename;
+    }
+
     private function _crondbs()
     {
-        $pathcron = "cron_dbbackup.php";
+        $cronfile = "cron_dbbackup.php";
+        $pathcron = $this->_get_cronpath($cronfile);
         echo "_crondbs(): $pathcron";
         if(is_file($pathcron)){
             include_once($pathcron);
