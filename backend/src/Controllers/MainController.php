3c15d1be6619024156a5455c4905758513cde8b8
ioedumac <eacevedof@yahoo.es>
2020-08-28 17:36:48 +0200

    maincontroller con _crondbs

--- backend/src/Controllers/MainController.php
+++ backend/src/Controllers/MainController.php
@@ -6,6 +6,7 @@ class MainController
     private function _crondbs()
     {
         $pathcron = "cron_dbbackup.php";
+        echo "_crondbs(): $pathcron";
         if(is_file($pathcron)){
             include_once($pathcron);
             (new CronDbbackup())->execute();
