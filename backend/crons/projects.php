a9d60f8f684bec2056de51024cb006779bddb679
ioedumac <eacevedof@yahoo.es>
2020-11-07 13:51:07 +0100

    projects

--- backend/crons/projects.php
+++
@@ -1,8 +0,0 @@
-<?php
-//scadado de /Users/ioedu/projects/prj_bash/py/config/projects.json
-//https://trello.com/c/iu5W0ldc/13-ionos-administrar-crons
-return [
-
-    "upload"=>[],
-    "tools"=>[],
-];
