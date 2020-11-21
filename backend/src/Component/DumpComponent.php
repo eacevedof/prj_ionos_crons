4da8cb4b0965776beac7d1e50525555126176baa
ioedumac <eacevedof@yahoo.es>
2020-11-21 21:40:11 +0100

    trazas y quito dos lineas por debajo

--- backend/src/Component/DumpComponent.php
+++ backend/src/Component/DumpComponent.php
@@ -21,9 +21,11 @@ class DumpComponent
         //-- Dump completed on 2020-11-21  3:15:07
         array_pop($arstr1);
         array_pop($arstr2);
-        //$c1 = count($arstr1);
-        //$c2 = count($arstr2);
-        //pr("file1:$c1, file2:$c2","dumpcomponent");
+        array_pop($arstr1);
+        array_pop($arstr2);
+        $c1 = count($arstr1);
+        $c2 = count($arstr2);
+        pr("file1:$c1, file2:$c2","dumpcomponent");
 
         $this->str1 = implode("\n",$arstr1);
         $this->str2 = implode("\n",$arstr2);
@@ -35,7 +37,7 @@ class DumpComponent
         $md1 = md5($this->str1); $this->str1 = null;
         $md2 = md5($this->str2); $this->str2 = null;
 
-        //pr("file1:$md1, file2:$md2","dumpcomponent");
+        pr("file1:$md1, file2:$md2","dumpcomponent");
         return $md1 === $md2;
     }
 }
