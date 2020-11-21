5ffc259acbc61a8b5cbc78c541aa873b3d74f061
ioedumac <eacevedof@yahoo.es>
2020-11-21 21:02:38 +0100

    trazas en dumpcomponent

--- backend/src/Component/DumpComponent.php
+++ backend/src/Component/DumpComponent.php
@@ -8,6 +8,7 @@ class DumpComponent
 
     public function __construct($path1, $path2)
     {
+        pr("file1:$path1, file2:$path2","dumpcomponent");
         $this->str1 = file_get_contents($path1);
         $this->str2 = file_get_contents($path2);
     }
@@ -20,6 +21,9 @@ class DumpComponent
         //-- Dump completed on 2020-11-21  3:15:07
         $arstr1 = array_pop($arstr1);
         $arstr2 = array_pop($arstr2);
+        $c1 = count($arstr1);
+        $c2 = count($arstr2);
+        pr("file1:$c1, file2:$c2","dumpcomponent");
 
         $this->str1 = implode("\n",$arstr1);
         $this->str2 = implode("\n",$arstr2);
@@ -30,6 +34,8 @@ class DumpComponent
         $this->_remove_lastline();
         $md1 = md5($this->str1); $this->str1 = null;
         $md2 = md5($this->str2); $this->str2 = null;
+
+        pr("file1:$md1, file2:$md2","dumpcomponent");
         return $md1 === $md2;
     }
 }
