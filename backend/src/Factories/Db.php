f479572435eff52c3372a9bbcdfb9a4959b26c1a
ioedumac <eacevedof@yahoo.es>
2020-11-26 21:42:19 +0100

    creando factories

---
+++ backend/src/Factories/Db.php
@@ -0,0 +1,10 @@
+<?php
+namespace App\Factories {
+
+    use App\Component\QueryComponent;
+
+    function db($context = "ipblocker")
+    {
+        return new QueryComponent($context);
+    }
+}
--- backend/src/Factories/db.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace App\Factories;
-
-use App\Component\QueryComponent;
-
-function db($context="ipblocker"){
-    return new QueryComponent($context);
-}
