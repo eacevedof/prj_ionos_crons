f9c0e12dad180750ff3cf5ebe751da8ce03f354d
ioedumac <eacevedof@yahoo.es>
2020-11-29 14:54:01 +0100

    email-component phpmail

--- backend/src/Services/Command/EmailService.php
+++ backend/src/Services/Command/EmailService.php
@@ -71,8 +71,8 @@ class EmailService extends ACommandService
     {
         $this->logpr("START EMAILSERVICE");
         error_reporting(E_ALL ^ E_NOTICE ^ E_DEPRECATED ^ E_STRICT);
-        $this->_send_smtp();  //todo ok con 1 solo attach
-        //$this->_send_phpmail();
+        //$this->_send_smtp();  //todo ok con 1 solo attach
+        $this->_send_phpmail();
         $this->logpr("END EMAILSERVICE");
     }
 }
