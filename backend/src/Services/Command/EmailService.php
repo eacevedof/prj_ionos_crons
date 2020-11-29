a3893daaf28bc377a11c372c74f3af4ca120f7e2
ioedumac <eacevedof@yahoo.es>
2020-11-29 12:26:29 +0100

    emailservice.php

--- backend/src/Services/Command/EmailService.php
+++ backend/src/Services/Command/EmailService.php
@@ -5,102 +5,38 @@ use function App\Functions\get_config;
 
 class EmailService extends ACommandService
 {
-    private function _pear()
+
+    private function _send_smtp()
     {
-
-        error_reporting(E_ALL ^ E_NOTICE ^ E_DEPRECATED ^ E_STRICT);
-
-        require_once "Mail.php";
-
-        $host = "ssl://smtp.dreamhost.com";
-        $username = "tucorreo@example.com";
-        $password = "la contraseña de tu correo";
-        $port = "465";
-        $email_from = "tucorreo@example.com";
-
-        $to = "correo_destinatario@example.com";
-        $email_subject = "Línea de asunto aquí:";
-        $email_body = "Lo que tu quieras";
-        $email_address = "responder-a@example.com";
-
-        $headers = array ('From' => $email_from, 'To' => $to, 'Subject' => $email_subject, 'Reply-To' => $email_address);
-        $smtp = \Mail::factory('smtp', array ('host' => $host, 'port' => $port, 'auth' => true, 'username' => $username, 'password' => $password));
-        $mail = $smtp->send($to, $headers, $email_body);
-
-
-        if (\PEAR::isError($mail)) {
-            echo("<p>" . $mail->getMessage() . "</p>");
-        } else {
-            echo("<p>Message successfully sent!</p>");
-        }
-    }
-
-    private function _send()
-    {
-        error_reporting(E_ALL ^ E_NOTICE ^ E_DEPRECATED ^ E_STRICT);
-        // Varios destinatarios
-        $para  = 'xxx@yahoo.es';// . ', '; // atención a la coma
-        //$para .= 'wez@example.com';
-
-        // título
-        $título = 'Recordatorio de cumpleaños para Agosto';
-
-        // mensaje
-        $mensaje = '
-        <html>
-        <head>
-        <title>Recordatorio de cumpleaños para Agosto</title>
-        </head>
-        <body>
-        <p>¡Estos son los cumpleaños para Agosto!</p>
-        <table>
-        <tr>
-        <th>Quien</th><th>Día</th><th>Mes</th><th>Año</th>
-        </tr>
-        <tr>
-        <td>Joe</td><td>3</td><td>Agosto</td><td>1970</td>
-        </tr>
-        <tr>
-        <td>Sally</td><td>17</td><td>Agosto</td><td>1973</td>
-        </tr>
-        </table>
-        </body>
-        </html>
-        ';
-
-        // Para enviar un correo HTML, debe establecerse la cabecera Content-type
-        $cabeceras  = 'MIME-Version: 1.0' . "\r\n";
-        $cabeceras .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
-
-        // Cabeceras adicionales
-        $cabeceras .= 'To: Mary <mary@example.com>, Kelly <kelly@example.com>' . "\r\n";
-        $cabeceras .= 'From: Recordatorio <cumples@example.com>' . "\r\n";
-        //$cabeceras .= 'Cc: birthdayarchive@example.com' . "\r\n";
-        //$cabeceras .= 'Bcc: birthdaycheck@example.com' . "\r\n";
-
-        // Enviarlo
-        mail($para, $título, $mensaje, $cabeceras);
-    }
-
-    public function run()
-    {
-        error_reporting(E_ALL ^ E_NOTICE ^ E_DEPRECATED ^ E_STRICT);
-        $this->logpr("START EMAILSERVICE");
+        $this->logpr("emailservice._send_smptp");
         $emails = get_config("emails");
-        $this->logpr($emails,"emails");
-        $r = (new EmailComponent($emails["config"][0]))
-            ->set_from($emails["config"][0]["email"])
+
+        $config = $emails["configs"][0];
+        $contact = $emails["contacts"][0];
+
+        $r = (new EmailComponent($config))
+            ->set_from($contact)
             ->add_to($emails["contacts"][0])
             ->set_subject("esto es un simple asunto")
             ->set_content("un contenido x")
             ->send()
             ->get_errors()
         ;
-        $this->logpr($r, "send result");
+        $this->logpr($r, "emailservice._send_smtp result");
+    }
 
-        //$email = new EmailComponent();
-        //$this->_send();
-        //$this->_pear();
+    private function _send_phpmail()
+    {
+        $this->logpr("emailservice._send_phpmail");
+        $this->logpr("emailservice._send_phpmail result");
+    }
+
+    public function run()
+    {
+        $this->logpr("START EMAILSERVICE");
+        error_reporting(E_ALL ^ E_NOTICE ^ E_DEPRECATED ^ E_STRICT);
+        $this->_send_smtp();
+        $this->_send_phpmail();
         $this->logpr("END EMAILSERVICE");
     }
 }
