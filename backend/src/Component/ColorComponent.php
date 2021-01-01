485d70ce6dc1b460962bb20cdbbaf26106f2aae3
ioedumac <eacevedof@yahoo.es>
2021-01-01 23:11:20 +0100

    aplica color

--- backend/src/Component/ColorComponent.php
+++ backend/src/Component/ColorComponent.php
@@ -1,13 +1,15 @@
 <?php
+// https://misc.flogisoft.com/bash/tip_colors_and_formatting
 namespace App\Component;
 
 class ColorComponent
 {
     //strreturn = "\033[{}m{}\033[00m".format(colcode,strval)
     private $colors = [
-        "default"   =>  "\033[39",
+        "default"   =>  "\033[39m",
         "white-1"   =>  "\033[0m",
         "red-1"     =>  "\033[31m",
+        "magenta"     =>  "\033[35m",
     ];
 
     private $texts = [];
