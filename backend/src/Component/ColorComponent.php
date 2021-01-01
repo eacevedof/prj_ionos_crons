bc7c9e23a96b06ccaa54911f6eb53734b0fc8774
ioedumac <eacevedof@yahoo.es>
2021-01-01 22:29:37 +0100

    color component

--- backend/src/Component/ColorComponent.php
+++ backend/src/Component/ColorComponent.php
@@ -3,15 +3,18 @@ namespace App\Component;
 
 class ColorComponent
 {
+    //strreturn = "\033[{}m{}\033[00m".format(colcode,strval)
     private $colors = [
         "white-1"=>"\033[0m",
         "red-1"=>"\033[31m",
     ];
+
     private $texts = [];
 
-    public function get_colored($text,$color="white-1"): string
+    public function get_colored($text, $color="white-1"): string
     {
-        return "";
+        $pre = $this->colors[$color];
+        return "$pre$text";
     }
 
     public function add($text, $color="white-1"): ColorComponent
