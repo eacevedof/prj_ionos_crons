883b8dbb43086aa7565f710be8a66ea798d5e238
ioedumac <eacevedof@yahoo.es>
2021-01-01 23:14:53 +0100

    nuevos colores

--- backend/src/Component/ColorComponent.php
+++ backend/src/Component/ColorComponent.php
@@ -7,21 +7,22 @@ class ColorComponent
     //strreturn = "\033[{}m{}\033[00m".format(colcode,strval)
     private $colors = [
         "default"       =>  "\033[39m",
-        "white-1"       =>  "\033[0m",
-        "red-1"         =>  "\033[31m",
+        "white"         =>  "\033[0m",
+        "red"           =>  "\033[31m",
+        "yellow"        =>  "\033[33m",
         "magenta"       =>  "\033[35m",
     ];
 
     private $texts = [];
 
-    public function get_colored(string $text, string $color="white-1"): string
+    public function get_colored(string $text, string $color="default"): string
     {
         $pre = $this->colors[$color];
         $def = $this->colors["default"];
         return "$pre$text$def";
     }
 
-    public function add(string $text, string $color="white-1"): ColorComponent
+    public function add(string $text, string $color="default"): ColorComponent
     {
         $this->texts[] = $this->get_colored($text,$color);
         return $this;
