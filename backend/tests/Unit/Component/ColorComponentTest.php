f94ecc64fa140c9cfe867bf355898c723fcaf932
ioedumac <eacevedof@yahoo.es>
2021-01-01 23:33:08 +0100

    se imprimen todos los colores

--- backend/tests/Unit/Component/ColorComponentTest.php
+++ backend/tests/Unit/Component/ColorComponentTest.php
@@ -12,6 +12,24 @@ class ColorComponentTest extends TestCase
 
     public function test_red()
     {
-        (new c())->add("hola","red-1")->pr();
+        (new c())
+            ->add("\n\nwhite\n","white")
+            ->add("red\n","red")
+            ->add("green\n","green")
+            ->add("yellow\n","yellow")
+            ->add("blue\n","blue")
+            ->add("magenta\n","magenta")
+            ->add("cyan\n","cyan")
+            ->add("dark gray\n","dark-gray")
+
+            ->add("light gray\n","light-gray")
+            ->add("light red\n","light-red")
+            ->add("light green\n","light-green")
+            ->add("light yellow\n","light-yellow")
+            ->add("light blue\n","light-blue")
+            ->add("light magenta\n","light-magenta")
+            ->add("light cyan\n","light-cyan")
+            ->add("light white\n","light-white")
+            ->pr();
     }
 }
