b0c0b302e5f8aacfff0e66f1d86435e802de52c1
ioedumac <eacevedof@yahoo.es>
2021-10-31 15:27:58 +0100

    fix blacklist

--- backend/src/Services/Command/Ipblocker/AddIpToBlacklist.php
+++ backend/src/Services/Command/Ipblocker/AddIpToBlacklist.php
@@ -19,7 +19,7 @@ final class AddIpToBlacklist extends ACommandService
         sleep(1);
         $sql = "
         INSERT INTO app_ip_blacklist(remote_ip, reason, is_blocked)
-        SELECT DISTINCT remote_ip,'cron - mlicious request',1
+        SELECT DISTINCT remote_ip,'cron - malicious request v2',1
         FROM app_ip_request
         WHERE 1
         AND insert_date > CURDATE()
@@ -33,7 +33,7 @@ final class AddIpToBlacklist extends ACommandService
             ) 
             OR (domain = 'elchalanaruba.com' AND get LIKE '{\"author\":\"%') 
         )
-        AND remote_ip NOT IN(
+        AND remote_ip NOT IN (
             SELECT DISTINCT remote_ip FROM app_ip_blacklist
         )
         ORDER BY remote_ip DESC
