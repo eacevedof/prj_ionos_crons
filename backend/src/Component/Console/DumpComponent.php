637cf8eb6c2be07acb6fbc16408a9b1f0ac14cc6
ioedumac <eacevedof@yahoo.es>
2022-04-21 22:54:18 +0200

    fix Allowed memory size of 134217728 bytes exhausted (tried to allocate 70034392 bytes) in Component/DumpComponent.php on line 13

--- backend/src/Component/Console/DumpComponent.php
+++ backend/src/Component/Console/DumpComponent.php
@@ -20,7 +20,7 @@ final class DumpComponent
 
     public function __construct($path1, $path2)
     {
-        $this->log([$path1, "vs", $path2],"compare","dumpcomponent");
+        $this->logpr([$path1, "vs", $path2],"compare","dumpcomponent");
         $this->pathtmp = getenv("HOME")."/mi_temporal";
         $this->path1 = $path1;
         $this->path2 = $path2;
@@ -41,19 +41,19 @@ final class DumpComponent
         //quita la ultima linea
         $cmd = "head -n -1 $this->pathcp1 > $this->pathtmp/tmp1.log";
         $r = cmd::exec($cmd);
-        $this->log($r,"_remove_dumpdate","dumpcomponent");
+        $this->logpr($r,"_remove_dumpdate","dumpcomponent");
 
         $cmd = "mv $this->pathtmp/tmp1.log $this->pathtmp/$this->name1";
         $r = cmd::exec($cmd);
-        $this->log($r,"_remove_dumpdate","dumpcomponent");
+        $this->logpr($r,"_remove_dumpdate","dumpcomponent");
 
         $cmd = "head -n -1 $this->pathcp2 > $this->pathtmp/tmp1.log";
         $r = cmd::exec($cmd);
-        $this->log($r,"_remove_dumpdate","dumpcomponent");
+        $this->logpr($r,"_remove_dumpdate","dumpcomponent");
 
         $cmd = "mv $this->pathtmp/tmp1.log $this->pathtmp/$this->name2";
         $r = cmd::exec($cmd);
-        $this->log($r,"_remove_dumpdate","dumpcomponent");
+        $this->logpr($r,"_remove_dumpdate","dumpcomponent");
     }
 
     private function _same_len(): bool
@@ -64,7 +64,7 @@ final class DumpComponent
         $cmd = "cat $this->pathcp2 | wc -m";
         $len2 = (int) cmd::exec($cmd)["exec"];
 
-        $this->log("l1:$len1, l2:$len2","same_len","dumpcomponent");
+        $this->logpr("l1:$len1, l2:$len2","same_len","dumpcomponent");
         return $len1 === $len2;
     }
 
@@ -76,7 +76,7 @@ final class DumpComponent
         $cmd = "cat $this->pathcp2 | md5sum";
         $md52 = cmd::exec($cmd)["exec"];
 
-        $this->log("m1:$md51, m2:$md52","_same_md5_of_content","dumpcomponent");
+        $this->logpr("m1:$md51, m2:$md52","_same_md5_of_content","dumpcomponent");
         return $md51 === $md52;
     }
 
@@ -84,7 +84,7 @@ final class DumpComponent
     {
         $cmd = "rm -f $this->pathcp1; rm -f $this->pathcp2";
         $r = cmd::exec($cmd);
-        $this->log($r,"_clean_temporal","dumpcomponent");
+        $this->logpr($r,"_clean_temporal","dumpcomponent");
     }
 
     public function are_thesame(): bool
